5df16d533bf9c3419a62aadfc8b8898554596074
Tafadzwa Chingore <tafadzwabchingore@gmail.com>
2025-04-14 11:45:36 -0700

    Headless Mode 4

--- tests/test_smokeTest.py
+++ tests/test_smokeTest.py
@@ -10,20 +10,15 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-from selenium.webdriver.firefox.options import Options
+#from selenium.webdriver.firefox.options import Options
 
- 
-
-class TestSmokeTestSuite():
+class TestSmokeTest():
 
   def setup_method(self, method):
-
-    opts = Options()
-
-    opts.add_argument("--headless")    
-
+    opts = FirefoxOptions()
+    opts.add_argument("--headless")
+    browser = webdriver.Firefox(options=opts)
     self.driver = webdriver.Firefox(options=opts)
-
     self.vars = {}
   
   def teardown_method(self, method):
