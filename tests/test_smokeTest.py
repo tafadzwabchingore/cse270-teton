464e335423215da433cf1ec1b78137d182e83f5a
Tafadzwa Chingore <tafadzwabchingore@gmail.com>
2025-04-14 11:34:19 -0700

    Headless Mode 2

--- tests/test_smokeTest.py
+++ tests/test_smokeTest.py
@@ -12,7 +12,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class TestSmokeTest():
   def setup_method(self, method):
-    opts = FirefoxOptions()
+    opts = FirefoxOptions() # type: ignore
     opts.add_argument("--headless")
     browser = webdriver.Firefox(options=opts)
     self.driver = webdriver.Firefox(options=opts)
